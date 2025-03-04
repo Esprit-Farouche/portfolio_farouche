676374ac471917c3998410eae835d16506fd87a2
Esprit-Farouche <romain.clair13@gmail.com>
2025-03-04 14:51:23 +0100

    Ajout Barba - Jquery - GSAP (1)
    Premiers ajouts de toutes ces librairies. Revenir a la version d'avant pour tout enlever.

--- Esprit_Farouche/public/scripts/script.js
+++ Esprit_Farouche/public/scripts/script.js
@@ -12,3 +12,50 @@ function raf(time) {
 requestAnimationFrame(raf);
 
 // ------------------
+
+
+// ------------------ BARBA.JS TRANSITIONS
+function pageTransition() {
+  var tl = gsap.timeline();
+  tl.to(".loading-screen", {
+    duration: 1.2,
+    width: "100%",
+    left: "0%",
+    ease: "Expo.easeInOut"
+  });
+
+  tl.to(".loading-screen", {
+    duration: 1,
+    width: "100%",
+    left: "100%",
+    ease: "Expo.easeInOut",
+    delay: 0.3
+  });
+  tl.set(".loading-screen", { left: "-100%" });
+}
+
+$(function() {
+  barba.init({
+    sync: true,
+
+    transitions: [
+      {
+        async leave(data) {
+          const done = this.async();
+
+          pageTransition();
+          await delay(1000);
+          done();
+        },
+        
+        async enter(data) {
+          contentAnimation();
+        },
+
+        async once(data) {
+          contentAnimation();
+        },
+      },
+    ],
+  });
+});
